a4e627be88f1b5f3ee1cc3494a5996f11b44704d
morishita <tomohiro.m0219@gmail.com>
2013-05-06 00:46:03 +0900

    rails4.0.0.rc1をインストール

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HitSaposenShiftManage::Application.config.secret_key_base = '0d0416af9afd19e2effd68bae71e6e3e98812046e70741290137d4528cab99d1765ae02981080a54426e51bb2ab2fabfe86831538c6e14085ab0b265dae7875e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HitSaposenShiftManage::Application.config.session_store :cookie_store, key: '_hit_saposen_shift_manage_session'
