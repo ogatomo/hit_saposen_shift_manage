7030ed49b3eb457570eb9a5dadf7076d54f780d1
morishita <tomohiro.m0219@gmail.com>
2013-05-09 14:59:12 +0900

    regularのfeatureのURLを日本語化

---
+++ features/step_definitions/regular_steps.rb
@@ -0,0 +1,71 @@
+もし(/^リーダー権限スタッフでログインしている$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^"(.*?)"にアクセスしている$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^必要項目を全て記述している$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^"(.*?)"ボタンを押す$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^レギュラーシフトを作成している$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^一般権限スタッフ以上の権限でログインしている$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^申請可能シフトが表示されている$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^希望の勤務時間を選択する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^申請した内容でDBに登録される$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^週２で希望の勤務時間を選択する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^エラーが表示される$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^任意のスタッフの申請を編集する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^編集結果を登録する$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^任意の"(.*?)"ボタンを押す$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^レギュラーシフトのconditionのフラグが更新され確定される$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^"(.*?)"に更新されたシフトが表示される$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+もし(/^任意のシフトを表示し"(.*?)"ボタンを押す$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+ならば(/^表示しているシフトをPDFでダウンロードできる$/) do
+  pending # express the regexp above with the code you wish you had
+end
